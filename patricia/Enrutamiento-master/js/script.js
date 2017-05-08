d34617ab4b5f8bb5897acbdc084cae2931641e3f
patricia125 <patriciaxitumul215@gmail.com>
2017-05-08 16:43:01 -0600

    Add files via upload

---
+++ patricia/Enrutamiento-master/js/script.js
@@ -0,0 +1,31 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // Home View
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/homePage.html'
+        })
+
+        // Info Vies
+        .state('about', {
+            url: '/about',
+            templateUrl: 'templates/info.html'
+        })
+        // Info Vies
+            .state('mismetas', {
+            url: '/mismetas',
+            templateUrl: 'templates/mismetas.html'
+        })
+       // Info Vies
+            .state('comosoy', {
+            url: '/comosoy',
+            templateUrl: 'templates/comosoy.html'
+        })
+
+});
